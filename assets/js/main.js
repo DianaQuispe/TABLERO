a9bc2bbf90f02f5ba0bc9848cf06c01e411ed4db
DianaQuispe <danaalienmas@gmail.com>
2017-08-03 15:51:10 -0500

    avance

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,20 @@
 var boton = document.getElementById('button');
 var numI = document.getElementById("input").value
 boton.onclick = function() {
+	var div = document.getElementById("tablero");
 	var numI = document.getElementById("input").value
-	var texto = document.createTextNode("sali");
-	var tabla = document.getElementById("tabla");  //llamo a mi tabla
+	var tabla = document.createElement("tabla");  //creo a mi tabla
+	tabla.setAttribute("bordecolor", "red");
 	for( var i =0;  i < numI; i++) {
-		var columna = document.createElement("tr"); // creo columnas
+		var fila = document.createElement("tr"); // creo columnas
 		for(var j =0; j< numI; j++) {
-			var celda = document.createElement("td"); //creo celda en filas
-			columna.appendChild(celda);
-		}
+			var z = document.createElement("td"); //creo z en filas
+			fila.appendChild(z); // pongo las filas en las columnas
+		} 
 			
-	tabla.appendChild(columna); // 
+	tabla.appendChild(fila); // 
+	tabla.appendChild(z);
 }
-borrar();
-} 
-function borrar() {
-		var tabla = document.getElementById("tabla"); 
-		table.setAttribute('bordercolor','red');
 
-}
+} 
+
